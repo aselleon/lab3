4b0f543337a2accc8f174eecfd4930513a3e18d3
temirlan248 <temirlan.muslimov.1@gmail.com>
2021-11-23 10:06:10 +0600

    lab6 changes = create form + htmls

---
+++ lab3_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.views.generic.edit import UpdateView
+from .models import *
+
+
+class AddShoesForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Shoes
+        fields = '__all__'
+
+
+class UpdateShoesForm(UpdateView):
+    model = Shoes
+    fields = '__all__'
+
